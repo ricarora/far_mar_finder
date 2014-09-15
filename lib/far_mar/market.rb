46d2505b2efd4147e85b88dcc6ee3ce80b7301a3
Richa <ricarora@gmail.com>
2014-09-15 16:30:47 -0700

    500 market object created

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -2,14 +2,24 @@ module FarMar
   class Market
     attr_reader :id, :market_name, :address, :city, :county, :state, :zip_code
 
+    def initialize (id, market_name, address, city, county, state, zip_code)
+      @id = id
+      @market_name = market_name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip_code = zip_code
+    end
+
     def self.all
       # returns all rows of the CSV file as objects
-      CSV.read("support/markets.csv").collect {|market| market = Market.new}
+      CSV.read("support/markets.csv").collect {|row| Market.new *row }
     end
 
     def self.find(id)
       # returns the row where the ID field matches the argument
-
+      CSV.read("support/markets.csv").collect {|row| }
     end
 
     def vendors
