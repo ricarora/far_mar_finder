e52c55aa8854a046841cf37ad5ed4d16e0e02320
Kathy Lepe <kathylepe434@gmail.com>
2014-09-19 14:03:42 -0700

    rubocop fixes 1

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -2,7 +2,7 @@ module FarMar
   class Market
     attr_reader :id, :name, :address, :city, :county, :state, :zip
 
-    def initialize (id, name, address, city, county, state, zip)
+    def initialize(id, name, address, city, county, state, zip)
       @id = id.to_i
       @name = name
       @address = address
@@ -10,59 +10,57 @@ module FarMar
       @county = county
       @state = state
       @zip = zip
-
     end
 
     def self.all
       # returns all rows of the CSV file as objects
-      CSV.read("support/markets.csv").collect {|row| Market.new *row }
+      CSV.read("support/markets.csv").collect { |row| Market.new *row }
     end
 
     def self.find(id)
       # returns the row where the ID field matches the argument
       # find method is finding one row for the matching id
       # then passed to market.new to convert as object
-      Market.all.find {|market| (market.id || "") == id }
+      Market.all.find { |market| (market.id || "") == id }
     end
 
     def vendors
       # returns a collection of FarMar::Vendor instances that are associated
       # with the market by the market_id field.
-      Vendor.all.find_all {|vendor| vendor.market_id == @id}
+      Vendor.all.select { |vendor| vendor.market_id == @id }
     end
 
     def products
       # returns a collection of FarMar::Product instances that are associated to
       # the market through the FarMar::Vendor class.
-      Product.all.find_all {|vendor|vendor.market == @market_id }
+      Product.all.select { |vendor|vendor.market == @market_id }
     end
 
     def self.search(search_term)
       # returns a collection of FarMar::Market instances where the market name or
       # vendor name contain the search_term. For example FarMar::Market.search('school')
       # would return 3 results, one being the market with id 75 (Fox School Farmers FarMar::Market).
-      Market.all.find_all {|market| market.name == search_term || market.vendors.name == search_term }
+      Market.all.select { |market| market.name == search_term || market.vendors.name == search_term }
     end
 
     def prefered_vendor
       # returns the vendor with the highest revenue
-      vendors.max_by {|vendor| vendor.revenue }
+      vendors.max_by { |vendor| vendor.revenue }
     end
 
     def prefered_vendor(date)
       # returns the vendor with the highest revenue for the given date
-      vendors.max_by {|vendor| vendor.revenue(date) }
+      vendors.max_by { |vendor| vendor.revenue(date) }
     end
 
     def worst_vendor
       # returns the vendor with the lowest revenue
-      vendors.min_by {|vendor| vendor.revenue }
+      vendors.min_by { |vendor| vendor.revenue }
     end
 
     def worst_vendor(date)
       # returns the vendor with the lowest revenue on the given date
-      vendors.min_by {|vendor| vendor.revenue(date) }
+      vendors.min_by { |vendor| vendor.revenue(date) }
     end
-
   end
 end
--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -10,7 +10,7 @@ module FarMar
 
     def self.all
       # returns all rows of the CSV file as objects
-      CSV.read("support/products.csv").collect {|row| Product.new *row }
+      CSV.read("support/products.csv").map { |row| Product.new *row }
     end
 
     def self.find(id)
@@ -18,13 +18,13 @@ module FarMar
       # find method is finding one row for the matching id
       # then passed to market.new to convert as object
       # Product.new *CSV.read("support/products.csv").find {|row| (row[0].to_i || "") == id }
-      Product.all.find {|product| (product.id || "") == id }
+      Product.all.find { |product| (product.id || "") == id }
     end
 
     def vendor
       # returns the FarMar::Vendor instance that is associated with this vendor
       # using the FarMar::Product vendor_id field
-      #Vendor.all.find {|vendor| vendor.id == @vendor_id}
+      # Vendor.all.find {|vendor| vendor.id == @vendor_id}
       Vendor.find(@vendor_id)
     end
 
@@ -34,22 +34,22 @@ module FarMar
       # market = Market.find(@vendor_id)
       # list_of_vendors = market.vendors
       # list_of_vendors.each {|each_vendor| (Sale.all.find_all {|sale| sale.product_id == @id})}
-      Sale.all.find_all {|sale| sale.product_id == @id}
+      Sale.all.select { |sale| sale.product_id == @id }
     end
 
     def number_of_sales
       # returns the number of times this product has been sold
-      Sale.all.find_all {|sale| sale.product_id == @id}
+      Sale.all.find_all { |sale| sale.product_id == @id }
     end
 
     def self.by_vendor(vendor_id)
       # returns all of the products with the given vendor_id
-      Product.all.find_all {|product| product.vendor_id == vendor_id}
+      Product.all.select { |product| product.vendor_id == vendor_id }
     end
 
     def self.most_revenue(n)
       # returns the top n products instances ranked by total revenue
-      Product.all.sort_by {|product| product.number_of_sales }.reverse.first(n)
+      Product.all.sort_by { |product| product.number_of_sales }.reverse.first(n)
     end
 
   end
