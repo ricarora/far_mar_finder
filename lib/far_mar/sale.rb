78dac8e0a8239d66d3c41bf22b7b017409e12848
Richa <ricarora@gmail.com>
2014-09-19 14:11:31 -0700

    cleaning code2

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -9,12 +9,11 @@ module FarMar
       @purchase_date = Date.parse(purchase_time)
       @vendor_id = vendor_id.to_i
       @product_id = product_id.to_i
-
     end
 
     def self.all
       # returns all rows of the CSV file as objects
-      CSV.read("support/sales.csv").collect {|row| Sale.new *row }
+      CSV.read("support/sales.csv").collect { |row| Sale.new *row }
     end
 
     def self.find(id)
@@ -22,7 +21,7 @@ module FarMar
       # find method is finding one row for the matching id
       # then passed to market.new to convert as object
       # Sale.new *CSV.read("support/sales.csv").find {|row| (row[0].to_i || "") == id }
-      Sale.all.find {|sale| (sale.id || "") == id }
+      Sale.all.find { |sale| (sale.id || '') == id }
     end
 
     def vendor
@@ -35,15 +34,12 @@ module FarMar
       # returns the FarMar::Product instance that is associated with this sale
       # using the FarMar::Sale product_id field
       Product.find(@product_id)
-
     end
 
     def self.between(beginning_time, end_time)
       # returns a collection of FarMar::Sale objects where the purchase time is
       # between the two times given as arguments
-      Sale.all.find_all {|sale| Time.parse(beginning_time) < sale.purchase_time  < Time.parse(end_time) }
-
+      Sale.all.find_all { |sale| Time.parse(beginning_time) < sale.purchase_time  < Time.parse(end_time) }
     end
-
   end
 end
