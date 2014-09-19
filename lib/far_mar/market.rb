2d5e22efc2179aecf07227d9b67c4f48e6643ffd
Kathy Lepe <kathylepe434@gmail.com>
2014-09-19 13:07:44 -0700

    cleaned up more

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -35,7 +35,6 @@ module FarMar
       # returns a collection of FarMar::Product instances that are associated to
       # the market through the FarMar::Vendor class.
       Product.all.find_all {|vendor|vendor.market == @market_id }
-
     end
 
     def self.search(search_term)
